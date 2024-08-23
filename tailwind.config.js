8d3ded1862a86c80854cda1c5ccfc36f9cf62e32
CentillionJinx <centillionjinx@gmail.com>
2024-08-24 01:18:22 +0800

    tailwind + DaisyUI + install

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,30 @@ export default {
 	theme: {
 		extend: {}
 	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#ebdbb2',
+
+					secondary: '#282828',
+
+					accent: '#fe8019',
+
+					neutral: '#1d2021',
+
+					'base-100': '#32302f',
+
+					info: '#83a598',
+
+					success: '#b8bb26',
+
+					warning: '#fabd2f',
+
+					error: '#fb4934'
+				}
+			}
+		]
+	},
 	plugins: [require('daisyui')]
 };
-
